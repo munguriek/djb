0a8db524d3233de90172f0b17eb9454506b6f67e
Otim Benjamin Munguriek <benjaminmunguriek@gmail.com>
2023-01-24 07:33:17 +0300

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+]
